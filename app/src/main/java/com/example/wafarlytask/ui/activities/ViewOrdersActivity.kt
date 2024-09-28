528512648f00699feb89b89e17cbe091026d9535
MuhameD <muhamed.roshdy007@gmail.com>
2024-09-28 19:19:36 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/wafarlytask/ui/activities/ViewOrdersActivity.kt
@@ -0,0 +1,19 @@
+package com.example.wafarlytask.ui.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wafarlytask.R
+
+class ViewOrdersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view_orders)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
---
+++ app/src/main/java/com/example/wafarlytask/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.wafarlytask.utils
+
+class Constants {
+
+}
